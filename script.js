1d8d0506fce6a74eefef4cd34817f74a3f295e11
Suman Das <sum4ndas@gmail.com>
2022-05-16 19:05:04 +0530

    Restart/Reset game with Reset/Restart button

--- script.js
+++ script.js
@@ -43,21 +43,21 @@ const Gameboard = (() => {
     let termCount = 0;
     const getGameWinner = () => {
         if(gameboard[0] && gameboard[0] === gameboard[1] && gameboard[0] === gameboard[2]) {
-            return (`${gameboard[0]} wins!`);
+            return (`${gameboard[0]} wins!0`);
         } else if (gameboard[0] && gameboard[0] === gameboard[3] && gameboard[0] === gameboard[6]) {
-            return (`${gameboard[0]} wins!`)
+            return (`${gameboard[0]} wins!0`)
         } else if (gameboard[0] && gameboard[0] === gameboard[4] && gameboard[0] === gameboard [8]) {
-            return (`${gameboard[0]} wins!`)
+            return (`${gameboard[0]} wins!0`)
         } else if (gameboard[1] && gameboard[1] === gameboard[4] && gameboard[1] === gameboard [7]) {
-            return (`${gameboard[1]} wins!`)
+            return (`${gameboard[1]} wins!1`)
         } else if (gameboard[2] && gameboard[2] === gameboard[5] && gameboard[2] === gameboard [8]) {
-            return (`${gameboard[2]} wins!`)
+            return (`${gameboard[2]} wins!2`)
         } else if (gameboard[2] && gameboard[2] === gameboard[4] && gameboard[2] === gameboard [6]) {
-            return (`${gameboard[2]} wins!`)
+            return (`${gameboard[2]} wins!2`)
         } else if (gameboard[3] && gameboard[3] === gameboard[4] && gameboard[3] === gameboard [5]) {
-            return (`${gameboard[3]} wins!`)
+            return (`${gameboard[3]} wins!3`)
         } else if (gameboard[6] && gameboard[6] === gameboard[7] && gameboard[6] === gameboard [8]) {
-            return (`${gameboard[6]} wins!`)
+            return (`${gameboard[6]} wins!6`)
         }
 
         termCount += 1;
@@ -89,14 +89,20 @@ const Gameboard = (() => {
         populateBoard();
         
     }
+
+    const restartGameClick = () => {
+        const restartBtn = document.querySelector('button');
+        restartBtn.addEventListener('click', restartGame);
+    }
     
 
     return {
         // populateBoard,
         playerPlay,
-        restartGame
+        restartGameClick,
     };
 })();
 
 // Gameboard.populateBoard();
 Gameboard.playerPlay();
+Gameboard.restartGameClick();
